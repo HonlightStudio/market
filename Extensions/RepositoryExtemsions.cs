9293ccb72a1b944d5489d843ee5c4ff5bd75f179
hossein <hosein8411@gmail.com>
2026-02-27 16:19:00 +0330

    Add RepositoryExtemsions

---
+++ Extensions/RepositoryExtemsions.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using CW10B.Repository;
+
+namespace CW10B.Extensions;
+
+public static class RepositoryExtemsions
+{
+    public static void CreateBackUp(this ProductRepository repository)
+    {
+        PersianCalendar calendar = new PersianCalendar();
+        string fileName = calendar.GetYear(DateTime.Now) + calendar.GetMonth(DateTime.Now).ToString() + calendar.GetDayOfMonth(DateTime.Now);
+        var data = repository.GetAll();
+        string json = JsonSerializer.Serialize(data);
+        File.WriteAllText(fileName, json);
+    }
+}
